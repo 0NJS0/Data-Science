2a178574330f1b5eaae4ae82c4ae6a244851ff81
Najmus Saquib <njsaurko3@gmail.com>
2025-01-15 01:27:50 +0600

    update

--- Final Project/project_topic_distribution.R
+++ Final Project/project_topic_distribution.R
@@ -1,21 +1,41 @@
+
 library(tm)
 library(topicmodels)
+library(SnowballC)
 
 
-corpus <- Corpus(VectorSource(tokens_df$tokens))
+file_path <- "D://Educational/AIUB sem 9/MID/Data Science/Project2.csv"
+data <- read.csv(file_path, stringsAsFactors = FALSE)
+
+corpus <- Corpus(VectorSource(data$tokens))
+
+
 dtm <- DocumentTermMatrix(corpus)
 
 
-row_sums <- rowSums(as.matrix(dtm))
-print(row_sums)
+tfidf <- weightTfIdf(dtm)
 
-dtm <- dtm[row_sums > 0,99 ]
+print(tfidf)
 
 
-k=3
+row_totals <- apply(dtm, 1, sum)
+dtm <- dtm[row_totals > 0, ] 
+k <- 5  
+
 lda_model <- LDA(dtm, k = k, control = list(seed = 123))
 
-# View top terms
-print(terms(lda_model,4))
+topic_term_weights <- posterior(lda_model)$terms
+
+print("Topics with token weights:")
+for (i in 1:k) {
+  cat(sprintf("\nTopic %d:\n", i))
+
+  token_weights <- sort(topic_term_weights[i, ], decreasing = TRUE)
+  top_tokens <- head(token_weights, 10)
+  
+  for (token in names(top_tokens)) {
+    cat(sprintf("%s (%.4f)\n", token, top_tokens[token]))
+  }
+}
 
 
